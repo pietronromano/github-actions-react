0fc75fc43bd435eee789c7ad6070d838ebd71bed
Pietro romano <pietronromano@live.com>
2025-10-09 14:35:16 +0200

    deploy-s3-javascript action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,3 +1,8 @@
+/* NOTE dependencies & .gitignore the workflows actions file needs to include ALL files
+cd .github/actions/deploy-s3-javascript
+npm install @actions/core @actions/exec @actions/github
+*/
+
 const core = require('@actions/core');
 // const github = require('@actions/github');
 const exec = require('@actions/exec');
@@ -8,9 +13,9 @@ function run() {
   const bucketRegion = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
 
-  // 2) Upload files
-  const s3Uri = `s3://${bucket}`;
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  // 2) Upload files: COMMENTED OUT FOR TESTING WITHOUT AWS
+  //const s3Uri = `s3://${bucket}`;
+  //exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
   core.setOutput('website-url', websiteUrl); // echo "website-url=..." >> $GITHUB_OUTPUT
